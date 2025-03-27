96be6f33e62f153135756c6a97859089f5412bfe
DariusTitiriga <andreas.titiriga@gmail.com>
2025-03-27 17:38:51 +0100

    Added unversioned files

---
+++ src/Game.java
@@ -0,0 +1,10 @@
+public class Game {
+    public static void main(String[] args) {
+        // Task 1 Declara o variabila int numarAscuns si salveaza in ea un numar oarecare de la 1 la 100
+        // Task 2 Cere de la user sa ghiceasca numarul ascuns
+        // Task 3 Daca numarul introdus de user este mare decat numarul ascuns afiseaza less daca e mai mic afiseaza more daca e egal afiseaza you guessed!
+
+
+
+    }
+}
